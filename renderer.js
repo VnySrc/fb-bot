2fb2ba36fb25df678518b3133a8e650cb5d1618d
VnySrc <vinyprog.work@gmail.com>
2022-12-10 20:46:49 -0300

    first commit

--- renderer.js
+++ renderer.js
@@ -507,7 +507,7 @@ async function getComments(page) {
       //  const commentPersonPicture = selecedComment.querySelector("i") // Nome da pessoa que comentou
         
         const uniqueID = selecedComment.querySelector(".x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1rg5ohu.x1a2a7pz")
-        const commentTag = selecedComment.querySelector("span.xv78j7m xt0e3qv")
+        const commentTag = selecedComment.querySelector("span.xv78j7m.xt0e3qv")
 
         window.getSelectedCommentPersonAndPicture(commentPersonName.innerHTML, uniqueID.getAttribute("href").split("user/")[1].split("/")[0], selectedCommentIndex, commentTag.innerHTML)
 
